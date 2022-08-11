1d5b27cde3738bf4341deb064f1da1abd5887419
arrKhange1 <therock65frostik@gmail.com>
2022-08-11 17:19:36 +0500

    feat: grow and shrink chats panel via border

--- telegram_client/src/components/Home/Home.tsx
+++ telegram_client/src/components/Home/Home.tsx
@@ -6,18 +6,23 @@ function Home() {
 
     return (
         <div className={classes.home}>
-            <div className={classes.chat_panel} onMouseDown={resize}> 
+            <div className={classes.chat_panel}> 
+                
                 <div className={classes.search}></div>
+                
                 <div className={classes.chats}>
                     {[...Array<string>(1000)].map((chat,i) => 
                         <div key={i}>#WELCOMETOCHATSВВВВВВВВВВВВВВВВВВВВВВВВВВВВВВВВВВВВВВВВ</div>
                     )}
                 </div>
             </div>
+            
+            <div className={classes.border} onMouseDown={resize}></div>
 
             <div className={classes.messages_panel}>
 
             </div>
+
         </div>
     );
 }
--- telegram_client/src/styles/home/home.module.css.d.ts
+++ telegram_client/src/styles/home/home.module.css.d.ts
@@ -2,4 +2,6 @@ export const home: string;
 export const chat_panel: string;
 export const messages_panel: string;
 export const chats: string;
+export const chats_wrapper: string;
+export const border: string;
 export const search: string;
