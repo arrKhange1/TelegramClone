34f2e283919d9ca6eb84b9263374bfcfbc3dd12d
arrKhange1 <therock65frostik@gmail.com>
2022-08-22 13:30:08 +0500

    feat: add search input as telegram has

--- telegram_client/src/components/Home/SidePanelHeader.tsx
+++ telegram_client/src/components/Home/SidePanelHeader.tsx
@@ -1,22 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IOption } from '../../@types/IOption';
 import side from '../../styles/side_panel/side.module.css';
 import CustomPopup from '../UI/CustomPopup/CustomPopup';
 import PopupBackArrow from '../UI/CustomPopup/PopupBackArrow';
+import CustomSearch from '../UI/CustomSearchInput/CustomSearch';
 
 function SidePanelHeader({selectedOption, setSelectedOption} :
      {setSelectedOption:React.Dispatch<React.SetStateAction<string>>,
     selectedOption: string}) {
 
+    const [isSearchActive, setIsSearchActive] = useState(false);
+    
     const options: IOption[] = [
         {name: 'Contacts'}
     ];
 
     return (
-        <div className={side.search}>
-            {selectedOption ? <PopupBackArrow setSelectedOption={setSelectedOption}/>  :
+        <div className={side.search} onClick={() => setIsSearchActive(false)}>
+            {selectedOption || isSearchActive ? <PopupBackArrow setSelectedOption={setSelectedOption}/>  :
                 <CustomPopup setSelectedOption={setSelectedOption} options={options}/>
             }
+            <CustomSearch isSearchActive={isSearchActive} setIsSearchActive={setIsSearchActive}/>
         </div>
     );
 }
