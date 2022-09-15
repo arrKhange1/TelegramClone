89ec1f351b7ad0ec7185fec453fb4c435ccfd193
arrKhange1 <therock65frostik@gmail.com>
2022-09-15 21:05:36 +0500

    feat: prepare chat controller and service

---
+++ Controllers/ChatsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TelegramClone.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ChatsController : ControllerBase
+    {
+        public IActionResult GetChat(string chatId, string initiatorId)
+        {
+            return Ok(new { chatId = chatId, initiator = initiatorId });
+        }
+    }
+}
