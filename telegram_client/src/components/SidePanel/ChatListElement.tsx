2291fe0aed025695fe68094d4cd1a89924529a49
arrKhange1 <therock65frostik@gmail.com>
2023-01-04 18:01:08 +0500

    feat: add proper last message senders in chatlist elements for better understanding (like You: msg... when u send a message)

--- telegram_client/src/components/SidePanel/ChatListElement.tsx
+++ telegram_client/src/components/SidePanel/ChatListElement.tsx
@@ -1,16 +1,37 @@
 import { useEffect, useState } from 'react';
 import IChat from '../../@types/IChat';
+import { useAuth } from '../../hooks/useAuth';
 import ContactIcon from '../../icons/ContactIcon';
 import side from '../../styles/side_panel/side.module.css';
 
 function ChatListElement({activeChat, chat} : {activeChat: string,
     chat: IChat}) {
     
+    const currUsername = useAuth().userName;
+
     function editLastMsgText(lastMsgText: string): string {
         if (lastMsgText.length > 20) 
             return lastMsgText.substring(0, 20) + '...';
         return lastMsgText;
     }
+
+    function getChatLastMessage() {
+        const msgType = chat.lastMessageType;
+        const sender = chat.lastMessageSender;
+        const shortenMessageText = editLastMsgText(chat.lastMessageText);
+        const chatType = chat.chatCategory;
+
+        if (msgType === "notification") 
+            return `${sender} ${shortenMessageText}`;
+        if (sender === currUsername) 
+            return `You: ${shortenMessageText}`;
+        if (chatType === "private")
+            return `${shortenMessageText}`;
+        if (chatType === "group") 
+            return `${sender}: ${shortenMessageText}`;
+            
+    }
+
 
     return (
         <div className={activeChat === chat.chatId ? `${side.chat} ${side.active}` :
@@ -18,8 +39,7 @@ function ChatListElement({activeChat, chat} : {activeChat: string,
             <ContactIcon className={side.chat_img}/>
             <div className={`${side.chat_content} ${side.chat_center}`}>
                 <div>{chat.chatName}</div>
-                <div>{chat.lastMessageType === "message" ? `${chat.lastMessageSender}: ${editLastMsgText(chat.lastMessageText)}` :
-                    `${chat.lastMessageSender} ${editLastMsgText(chat.lastMessageText)}`}</div>
+                <div>{getChatLastMessage()}</div>
             </div>
             {chat.unreadMsgs ? <div className={`${side.chat_content} ${side.chat_end}`}>{chat.unreadMsgs}</div> : <></>}
         </div>
